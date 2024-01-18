4b443c9409a183af0fb8672ede71027a991076c7
biny <>
2024-01-18 23:22:05 +0800

    update type

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,28 +1,29 @@
 import {Middleware} from "redux";
 
-declare module 'next-redux-localstorage-simple' {
+declare module 'redux-localstorage-simple-next' {
   interface RLSOptions {
-    states?: string[];
-    ignoreStates?: string[];
-    namespace?: string;
-    namespaceSeparator?: string;
-    debounce?: number;
-    disableWarnings?: boolean;
+    states?: string[]
+    ignoreStates?: string[]
+    namespace?: string
+    namespaceSeparator?: string
+    debounce?: number
+    disableWarnings?: boolean
   }
   interface LoadOptions {
-    states?: string[];
-    immutablejs?: boolean;
-    namespace?: string;
-    namespaceSeparator?: string;
-    preloadedState?: {};
-    disableWarnings?: boolean;
+    states?: string[]
+    immutablejs?: boolean
+    namespace?: string
+    namespaceSeparator?: string
+    // eslint-disable-next-line @typescript-eslint/ban-types
+    preloadedState?: {}
+    disableWarnings?: boolean
   }
   interface ClearOptions {
-    namespace?: string;
-    disableWarnings?: boolean;
+    namespace?: string
+    disableWarnings?: boolean
   }
-  export function save(options?:RLSOptions):Middleware
-  export function load(options?:LoadOptions):object
-  export function clear(options?:ClearOptions):void
-  export function combineLoads(...loads:object[]):object
+  export function save(options?: RLSOptions): Middleware
+  export function load(options?: LoadOptions): object
+  export function clear(options?: ClearOptions): void
+  export function combineLoads(...loads: object[]): object
 }
